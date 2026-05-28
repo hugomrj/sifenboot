de132b5b10a70df31ef3db8d20896907ef17f081
hugo romero <hugomrj@gmail.com>
2026-05-28 12:28:07 -0300

    Agregar validación X-API-KEY en consulta RUC

--- src/main/java/org/sifenboot/core/ruc/controller/ConsultaRucController.java
+++ src/main/java/org/sifenboot/core/ruc/controller/ConsultaRucController.java
@@ -3,6 +3,8 @@ package org.sifenboot.core.ruc.controller;
 import org.sifenboot.core.ruc.dto.request.ConsultaRucRequest;
 import org.sifenboot.core.ruc.service.ConsultarRucService;
 import jakarta.validation.Valid;
+import org.sifenboot.errors.UnauthorizedException;
+import org.sifenboot.security.api.TokenService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,15 +14,24 @@ import org.springframework.web.bind.annotation.*;
 public class ConsultaRucController {
 
     private final ConsultarRucService consultarRucService;
+    private final TokenService tokenService;
 
-    public ConsultaRucController(ConsultarRucService consultarRucService) {
+    public ConsultaRucController(ConsultarRucService consultarRucService,
+                                 TokenService tokenService ) {
         this.consultarRucService = consultarRucService;
+        this.tokenService = tokenService;
     }
 
     @PostMapping("/consulta/ruc")
     public ResponseEntity<?> consultarRuc(
             @PathVariable String emisor,
+            @RequestHeader(value = "X-API-KEY", defaultValue = "") String token,
             @Valid @RequestBody ConsultaRucRequest request) {
+
+        if (!tokenService.esTokenValido(emisor, token)) {
+            throw new UnauthorizedException("Token inválido o no autorizado para el emisor: "
+                    + emisor);
+        }
 
         var result = consultarRucService.consultar(emisor, request.getRuc());
         return ResponseEntity.ok(result);
